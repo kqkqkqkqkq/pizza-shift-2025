fe531929831927a606d7b7c38979f7865c5b1933
YungOleg <bilk5899@gmail.com>
2025-02-13 12:06:33 +0700

    enable edge to edge

--- app/src/main/java/dev/k/shift/MainActivity.kt
+++ app/src/main/java/dev/k/shift/MainActivity.kt
@@ -1,15 +1,25 @@
 package dev.k.shift
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT, darkScrim = Color.TRANSPARENT,
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT, darkScrim = Color.TRANSPARENT,
+            )
+        )
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContent {
             Navigation()
         }
